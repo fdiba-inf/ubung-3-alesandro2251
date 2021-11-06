e0a9d9abe030d4696e6ceb2a38606814f56cb563
alesandro2251 <>
2021-11-06 13:55:52 +0000

    Some message

--- src/exercise3/AngleCalculation.java
+++ src/exercise3/AngleCalculation.java
@@ -7,20 +7,18 @@ public class AngleCalculation{
   public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int winkel = input.nextInt();
-        double r = 0;
-        double g = 0;
+        double winkel = input.nextDouble();
         char ch = input.next().charAt(0);
 
 
         do{
           if(ch == 'r'){
-            r= (winkel * Math.PI)/180;
-            System.out.println("Angle: " + g + ch);
+             double r = (winkel * Math.PI)/180;
+            System.out.println("Angle: " + r );
           }
           else{
-            g= (winkel*180)/Math.PI;
-            System.out.println("Angle: "+ r + ch);
+          double g= (winkel*180)/Math.PI;
+            System.out.println("Angle: "+ g );
           }
         ch = input.next().charAt(0);
         }while((ch == 'r')|| (ch == 'd'));
